0d4a6390d97a616bed234aa5938249cec887e8f7
QiJing Chen <qijing@wo.cn>
2021-01-23 17:31:23 +0800

    移除已过期的方法

--- www/cordova-plugin-flutter.js
+++ www/cordova-plugin-flutter.js
@@ -13,33 +13,5 @@ Flutter.prototype.open = function (routerName, success, error) {
     exec(success, error, 'CordovaFlutter', 'open', routerNameParams);
 };
 
-Flutter.prototype.invokeCallback = function (callbackData, uuidMethod, success, error) {
-    var result = {
-        result: callbackData,
-        uuid: uuidMethod
-    };
-    exec(success, error, "CordovaFlutter", "invokeCallback", [result]);
-}
-
-if (window.bridgeFlutter == null) {
-    window.bridgeFlutter = {};
-}
-
-window.bridgeFlutterInvoke = function (uuid, methodName, args) {
-    if (window.bridgeFlutter[methodName] == null) {
-        this.alert("window.bridgeFlutter." + methodName + " 方法不存在");
-        return;
-    }
-    window.bridgeFlutter[methodName](args, function (result) {
-        var uuidMethod = methodName + "#" + uuid + "#";
-        flutter.invokeCallback(result, uuidMethod, function () {
-
-        }, function () {
-
-        });
-    });
-}
-
-
 module.exports = new Flutter();
 
